9e9b9af5a4c092ac35a9f308a8d35a7d59b61bcf
z80 <bashkirov.sergey@gmail.com>
2013-10-09 20:56:20 +0400

    Python binary modules are opened by Python, rearranged includes and tests for Pawn.

--- main-board/software/pawn/module.py.i2c/src/supply_py.cpp
+++ main-board/software/pawn/module.py.i2c/src/supply_py.cpp
@@ -3,7 +3,7 @@
 #include <boost/python.hpp>
 using namespace boost::python;
 
-BOOST_PYTHON_MODULE( supply )
+BOOST_PYTHON_MODULE( supplyctrli2c )
 {
     // Create the Python type object for our extension class and define __init__ function.
     class_<Supply>("Supply", init<>())
