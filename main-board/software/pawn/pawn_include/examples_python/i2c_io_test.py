39de72a979faedf1620cebd08deadd3d376ab3dd
z80 <bashkirov.sergey@gmail.com>
2013-10-21 22:38:00 +0400

    Forgotten to add i2c test.

---
+++ main-board/software/pawn/pawn_include/examples_python/i2c_io_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys
+from time import sleep
+from supplyctrli2c import *
+
+class A:
+    pwm = 0
+
+    def __init__( self ):
+        self.pwm = 0
+	self.io = Supply()
+
+    def setPwm( self, arg ):
+        if ( arg < 0 ):
+	    arg = 0;
+	if ( arg > 255 ):
+	    arg = 255
+    	res = self.io.setIo( 0, arg )
+	return res
+
+
+a = A()
+t = 0.1
+
+for k in range( 5 ):
+    for i in range( 50, 200, 10 ):
+        a.setPwm( i )
+        sleep( t )
+
+
+
