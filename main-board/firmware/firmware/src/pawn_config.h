43345eeb66950cf81d091b494822d9c78888af71
z80 <bashkirov.sergey@gmail.com>
2013-10-27 16:07:27 +0400

    Made DFU size #define constant.

--- main-board/firmware/firmware/src/pawn_config.h
+++ main-board/firmware/firmware/src/pawn_config.h
@@ -11,9 +11,12 @@
 #define PAWN_PAGE_SIZE     1024
 // Start FLASH block to keep pawn program in.
 #define PAWN_START_PAGE    38
+// At the very end of FLASH memory there is a DFU firmware.
+// This is it's size in FLASH pages.
+#define PAWN_DFU_SIZE      10
 // Pawn permitted pages cnt.
 // Here 128 is total pages cnt, 10 is DFU firmware at the very end of FLASH size.
-#define PAWN_PAGES_CNT     ( 128 - PAWN_START_PAGE - 10 )
+#define PAWN_PAGES_CNT     ( 128 - PAWN_START_PAGE - PAWN_DFU_SIZE )
 // Start of FLASH memory
 #define PAWN_FLASH_START   0x08000000
 // Chibios PAWN thread stack depth.
