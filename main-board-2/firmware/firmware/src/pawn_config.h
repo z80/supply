35ab4f2d7b45728fc0e522ecbe9b96e8eff4a020
z80 <bashkirov.sergey@gmail.com>
2013-12-14 23:00:28 +0400

    Adjusted Pawn settings.

--- main-board-2/firmware/firmware/src/pawn_config.h
+++ main-board-2/firmware/firmware/src/pawn_config.h
@@ -10,13 +10,11 @@
 // FLASH parameters.
 #define PAWN_PAGE_SIZE     1024
 // Start FLASH block to keep pawn program in.
-#define PAWN_START_PAGE    38
-// At the very end of FLASH memory there is a DFU firmware.
-// This is it's size in FLASH pages.
-#define PAWN_DFU_SIZE      10
+#define PAWN_START_PAGE      48
+#define PAWN_FLASH_PAGES_CNT 128
 // Pawn permitted pages cnt.
 // Here 128 is total pages cnt, 10 is DFU firmware at the very end of FLASH size.
-#define PAWN_PAGES_CNT     ( 128 - PAWN_START_PAGE - PAWN_DFU_SIZE )
+#define PAWN_PAGES_CNT     ( PAWN_FLASH_PAGES_CNT - PAWN_START_PAGE )
 // Start of FLASH memory
 #define PAWN_FLASH_START   0x08000000
 // Chibios PAWN thread stack depth.
